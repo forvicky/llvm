bad1fddf5ce977013a6dd633d61e939e6368c268
zdd <zdd2389@gmail.com>
2020-09-22 20:12:21 +0800

    first commit

---
+++ llvm/lib/Transforms/EncodeFunctionName/EncodeFunctionName.cpp
@@ -0,0 +1,50 @@
+//
+// Created by root on 9/22/20.
+//
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+namespace {
+struct EncodeFunctionName : public FunctionPass {
+  static char ID;
+  EncodeFunctionName() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function &F) override {
+    errs() << "EncodeFunctionName: "+F.getName()+" -> ";
+    if(F.getName().compare("main")!=0){
+
+      llvm::MD5 Hasher;
+      llvm::MD5::MD5Result Hash;
+      Hasher.update("zdd_");
+      Hasher.update(F.getName());
+
+      Hasher.final(Hash);
+
+      SmallString<32> HexString;
+      llvm::MD5::stringifyResult(Hash, HexString);
+
+
+      F.setName(HexString);
+    }
+
+    errs().write_escaped(F.getName()) << '\n';
+    return false;
+  }
+}; // end of struct Hello
+}  // end of anonymous namespace
+
+char EncodeFunctionName::ID = 0;
+static RegisterPass<EncodeFunctionName> X("encode", "Encode Function Name Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
+
+static RegisterStandardPasses Y(
+    PassManagerBuilder::EP_EarlyAsPossible,
+    [](const PassManagerBuilder &Builder,
+       legacy::PassManagerBase &PM) { PM.add(new EncodeFunctionName()); });
